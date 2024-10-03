a2db790b2adbfbd4e1ee2c68392de7ecc88510d3
michaelanbrown <m_brownbrown@yahoo.com>
2024-10-03 16:24:29 -0400

    trying removing comma

--- lib/regex.py
+++ lib/regex.py
@@ -9,5 +9,5 @@ name_regex = re.compile(name)
 phone_number = r"\({0,1}[\d]{3}\){0,1}[- ]{0,1}[\d]{3}-{0,1}[\d]{4}"
 phone_regex = re.compile(phone_number)
 
-email_address = r"[A-z]([A-z0-9][\.]{0,1})+[A-z0-9]+@[A-z0-9]+\.[a-z]{2,}"
+email_address = r"[A-z]([A-z0-9][\.]{0,1})+[A-z0-9]+@[A-z0-9]+\.[a-z]{2}"
 email_regex = re.compile(email_address)
